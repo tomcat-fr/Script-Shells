1708162423617ac1eeee9b12ccf4d2ec71ee0d19
tomcat-fr <tomcat@snowcat.fr>
2017-04-18 11:39:10 +0200

    Ajout check SSM sur le script

--- ux_report.sh
+++ ux_report.sh
@@ -3,4 +3,12 @@
 # Auteur : tomcat@snowcat.fr
 # Avril 2017
 
+echo 'Reporting Espace Disque' > /home/tomcat/Documents/sysrep/ux_report.txt
+echo >> /home/tomcat/Documents/sysrep/ux_report.txt
+ssm list >> /home/tomcat/Documents/sysrep/ux_report.txt
+echo >> /home/tomcat/Documents/sysrep/ux_report.txt
+
+
+
+
 
