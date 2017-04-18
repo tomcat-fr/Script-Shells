2a2080a1f1772a8035e1db2a442c578c8422a072
tomcat-fr <tomcat@snowcat.fr>
2017-04-18 11:54:57 +0200

    Script de reporting version 1.0

--- ux_report.sh
+++ ux_report.sh
@@ -3,10 +3,37 @@
 # Auteur : tomcat@snowcat.fr
 # Avril 2017
 
+# Reporting Espace Disque
+
 echo 'Reporting Espace Disque' > /home/tomcat/Documents/sysrep/ux_report.txt
 echo >> /home/tomcat/Documents/sysrep/ux_report.txt
 ssm list >> /home/tomcat/Documents/sysrep/ux_report.txt
 echo >> /home/tomcat/Documents/sysrep/ux_report.txt
+
+# Reporting Process en cours
+
+echo >> /home/tomcat/Documents/sysrep/ux_report.txt
+ps -ef >> /home/tomcat/Documents/sysrep/ux_report.txt
+echo >> /home/tomcat/Documents/sysrep/ux_report.txt
+
+# reporting Etat des Process
+
+monit summary >> /home/tomcat/Documents/sysrep/ux_report.txt
+echo >> /home/tomcat/Documents/sysrep/ux_report.txt
+
+# Check SuriCata fast.log (50 dernière lignes)
+
+tail -50 /var/log/suricata/fast.log >> /home/tomcat/Documents/sysrep/ux_report.txt
+echo >> /home/tomcat/Documents/sysrep/ux_report.txt
+
+# Reporting Fail2ban
+
+tail -30 /var/log/fail2ban.log >> /home/tomcat/Documents/sysrep/ux_report.txt
+echo >> /home/tomcat/Documents/sysrep/ux_report.txt
+
+# Envoi du mail de reporting
+
+mail -s "Reporting Journalier Snowcat-UX" tomcat@snowcat.fr < /home/tomcat/Documents/sysrep/ux_report.txt
 
 
 
